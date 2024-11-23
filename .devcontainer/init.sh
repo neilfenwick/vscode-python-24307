d679d72a605bd446cede36afb13dc1b639a5b01c
Neil Fenwick <neil.fenwick@gmail.com>
2024-11-23 23:48:05 +0000

    Create devcontainer

---
+++ .devcontainer/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://files.pythonhosted.org/packages/95/aa/8db7154833bfea92cddee249d53a102f0fd9da8f3943a62f012f97b37e9a/dotnetcore2-3.1.23-py3-none-win_amd64.whl
+pip install --upgrade pip
+pip install mltable pytest
